6ac8a16acf676416c6f29c8dbccfaa247115ed86
Alexander Shopov <ashopov@uber.com>
2017-03-22 17:37:16 +0200

    Move to thrift 0.9.3

--- build/travis/install-thrift.sh
+++ build/travis/install-thrift.sh
@@ -4,8 +4,8 @@ set -x
 
 mkdir -p "$THRIFT_PREFIX"
 
-wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz
-tar -xzvf thrift-0.9.2.tar.gz
-cd thrift-0.9.2
+wget http://archive.apache.org/dist/thrift/0.9.3/thrift-0.9.3.tar.gz
+tar -xzvf thrift-0.9.3.tar.gz
+cd thrift-0.9.3
 ./configure --prefix="$THRIFT_PREFIX" --enable-libs=no --enable-tests=no --enable-tutorial=no
 make -j2 && make install
